47a45196d4973538c3af0a1b0deb0961149e6d05
M A Monim <ma.monim35@gmail.com>
2021-06-23 16:38:35 +0600

    Add files via upload

---
+++ assets/js/script.js
@@ -0,0 +1,101 @@
+/**
+ * @Script js for (Template/Project Name)
+ *
+ * @project     - Project Name
+ * @author      - 
+ * @created_by  - 
+ * @created_at  - 
+ * @modified_by -
+ */
+
+
+/**
+ * ========================================================
+ * this function execute when window properly loaded
+ * ===========================================================
+ */
+
+$(window).on('load', function () {
+
+    // code should be execute here
+
+});
+
+
+
+/**
+ * ========================================================
+ * this function execute when DOM element ready 
+ * ===========================================================
+ */
+
+$(document).ready(function () {
+
+    // for select#select-country
+    $(function () {
+        if ($('#select-lang').length) {
+            function custom_template(obj) {
+                var data = $(obj.element).data();
+                var text = $(obj.element).text();
+                if (data && data['img_src']) {
+                    img_src = data['img_src'];
+                    template = $("<span class='img-option'><img src=\"" + img_src + "\" style=\"width:20px;height:12px;\" /> " + text + "</span>");
+                    return template;
+                }
+            }
+            $('#select-lang').select2({
+                minimumResultsForSearch: -1,
+                'templateSelection': custom_template,
+                'templateResult': custom_template,
+            });
+        }
+    });
+
+
+    // parallax with jarallax
+    $(function () {
+        if ($('.pricing-switch').length) {
+            let lightPlan = $('.light-plan').find('.widget-price').text();
+            let proPlan = $('.pro-plan').find('.widget-price').text();
+            let businessPlan = $('.business-plan').find('.widget-price').text();
+            $('#pricing-switch-input').on('change', function (e) {
+                if ($(this).is(':checked')) {
+                    $('.light-plan').find('.widget-price').text('$449.99');
+                    $('.pro-plan').find('.widget-price').text('$549.99');
+                    $('.business-plan').find('.widget-price').text('$699.99');
+
+                    $('.billed-monthly').removeClass('active');
+                    $('.billed-annually').addClass('active');
+                } else {
+                    $('.light-plan').find('.widget-price').text(lightPlan);
+                    $('.pro-plan').find('.widget-price').text(proPlan);
+                    $('.business-plan').find('.widget-price').text(businessPlan);
+
+                    $('.billed-monthly').addClass('active');
+                    $('.billed-annually').removeClass('active');
+                }
+            });
+        }
+    });
+
+
+    
+    // parallax with jarallax
+    $(function () {
+        if ($('.jarallax').length) {
+            jarallax(document.querySelectorAll('.jarallax'));
+        }
+    });
+
+
+    // wow js init
+    $(function () {
+        var wow = new WOW({
+            animateClass: 'animated',
+            mobile: true
+        });
+        wow.init();
+    });
+
+
+});
